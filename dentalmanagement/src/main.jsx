f0cfd65e57efb0a31ea11c2673c3cae041a720c2
trixie <trixiegailcloma24@gmail.com>
2024-04-17 13:23:51 +0800

    added images folder

--- dentalmanagement/src/main.jsx
+++ dentalmanagement/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import CreateNurseAccount from './Components/CreateNurseAccount'
-import CreateDoctorAccount from './Components/CreateDoctorAccount'
-import CreateStaffAccount from './Components/CreateStaffAccount'
-// import CreateStudentAccount from './Components/CreateStudentAccount.jsx';
+// import CreateNurseAccount from './Components/CreateNurseAccount'
+// import CreateDoctorAccount from './Components/CreateDoctorAccount'
+// import CreateStaffAccount from './Components/CreateStaffAccount'
+import CreateStudentAccount from './Components/CreateStudentAccount.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <CreateStudentAccount /> */}
-    <CreateStaffAccount />
+    <CreateStudentAccount />
+    {/* <CreateStaffAccount />
     <CreateDoctorAccount />
-    <CreateNurseAccount />
+    <CreateNurseAccount /> */}
 
   </React.StrictMode>,
 )
